78e323a323b7ada731a679b4b3154ca22f22eb0d
anhtaka <royal.norton@gamil.com>
2021-05-01 21:21:37 +0900

    add

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var moment = require("moment");
+
+// 現在時刻を取得する例
+console.log(moment().format()); // 2014-07-16T09:00:00+09:00 (default)
+console.log(moment().format("YYYY-MM-DD"));          // 2014-07-17
+
 const url = 'https://anhtaka.github.io/holiday-node/holiday-main.json';
 
 const https = require('https');
