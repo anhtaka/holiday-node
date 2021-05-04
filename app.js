c1e3388ad8de9e05b5c2b10b8f39a54c0adf87a1
anhtaka <royal.norton@gamil.com>
2021-05-04 20:37:41 +0900

    add

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var request = require('sync-request');
+var moment = require("moment");
+
 const getUrl = 'https://anhtaka.github.io/holiday-node/holiday-main.json';
 var AryHoliday =[];
 var returnCode;
 
-
+//exports.handler = function (event, context) {
 console.log("Start  Return Request Sync");
 returnCode = httpGet(getUrl);
 console.log("Status Code (main)     : "+returnCode);
 console.log(AryHoliday);
 console.log("End    Return Request Sync");
+
+
+//}
 
 function httpGet(url){
   var response = request(
@@ -22,4 +27,21 @@ function httpGet(url){
         AryHoliday.push(obj.holiday[item].DATA);
     }
     return response.statusCode;
+}
+/*  input:yyyy-mm-dd  */
+function checkweekMonFriHolidayHoliday(value) {
+  var flg = 0;
+  switch (value) {
+      case 'Monday':
+      case 'Tuesday':
+      case 'Wednesday':
+      case 'Thursday':
+      case 'Friday':
+          //console.log("checkweekMonFri = Mon-Fri");
+          return 1;
+      case 'Saturday':
+      case 'Sunday':
+          //console.log("checkweekMonFri = Sat-Sun");
+          return 0;
+  }
 }
