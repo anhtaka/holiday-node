5eb7664996454b021ee9faf2b8eb6cb0d56f96b8
anhtaka <royal.norton@gamil.com>
2021-05-04 20:57:47 +0900

    add

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var request = require('sync-request');
 var moment = require("moment");
-
+var NOWDATE;
+NOWDATE = getNow();
+//---------
 const getUrl = 'https://anhtaka.github.io/holiday-node/holiday-main.json';
 var AryHoliday =[];
 var returnCode;
@@ -12,7 +14,11 @@ console.log("Status Code (main)     : "+returnCode);
 console.log(AryHoliday);
 console.log("End    Return Request Sync");
 
-
+if (chkHolidayHoliday(NOWDATE) === 1) {
+  console.log("holiday");
+}else{
+  console.log("work"); 
+}
 //}
 
 function httpGet(url){
@@ -29,19 +35,35 @@ function httpGet(url){
     return response.statusCode;
 }
 /*  input:yyyy-mm-dd  */
-function checkweekMonFriHolidayHoliday(value) {
+function chkHolidayHoliday(valueDate) {
   var flg = 0;
-  switch (value) {
+　console.log('dddd'+ valueDate.format('dddd'));
+  switch (valueDate.format('dddd')) {
       case 'Monday':
       case 'Tuesday':
       case 'Wednesday':
       case 'Thursday':
       case 'Friday':
-          //console.log("checkweekMonFri = Mon-Fri");
-          return 1;
+        console.log(valueDate.format('YYYY-MM-DD'));
+          //holiday検索
+        var a = AryHoliday.indexOf(valueDate.format('YYYY-MM-DD'));
+        console.log("a=" + a);
+        if(a == -1){
+          flg =  0;
+        }else{
+          flg = 1; //holiday
+        }
+        
+        break;
       case 'Saturday':
       case 'Sunday':
-          //console.log("checkweekMonFri = Sat-Sun");
-          return 0;
+        flg = 1;
+        break;
   }
+  console 
+  return flg
+}
+function getNow() {
+  //console.log("TEST");
+  return moment().utcOffset("+09:00");
 }
